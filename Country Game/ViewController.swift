64dc42196a00653db7beedd1a89d9bc0108ced86
Nikolai Saganenko <9952728@gmail.com>
2021-10-22 15:51:40 +0300

    add button images add ask() function

--- Country Game/ViewController.swift
+++ Country Game/ViewController.swift
@@ -12,9 +12,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        ask()
+    }
+    
+    func ask() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[0]), for: .normal)
+        button3.setImage(UIImage(named: countries[0]), for: .normal)
     }
 
 
